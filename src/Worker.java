8094cd12694bab8bfc905d3605c0dd2e5a5215ee
Сергей <sharker063@gmail.com>
2023-07-04 23:56:21 +0400

    курсовая переделаная

--- src/Worker.java
+++ src/Worker.java
@@ -1,79 +1,59 @@
 public class Worker {
-        private String fullName;
-        private String name;
-        private String surname;
-        private String midname;
-        private int otdel;
-        private int salary;
-        private double salary;
-        public static int idCount = 1; // id
-        private int id;
+    private String fullName;
+    private int otdel;
+    private int salary;
+    private int id;
 
-        public Worker(String fullName, int otdel, int salary) {
-            this.fullName = fullName;
-    public Worker(String name, String surname, String midname, int otdel, double salary){
-                this.name = name;
-                this.surname = surname;
-                this.midname = midname;
-                this.otdel = otdel;
-                this.salary = salary;
-                this.id = idCount;
-                idCount++;
-                this.id = idCount++;
-            }
-            private Worker[] worker;
-    public Worker() {
-                int[] worker = new int[10];
+    private static int idCount = 1; // id
 
-                public String getName () {
-                    return name;
-                }
-                public String getSurname () {
-                    return surname;
-                }
-                public String getMidname () {
-                    return midname;
-                }
-                public String fullName () {
-                    return fullName;
-                    public int getOtdel () {
-                        return otdel;
-                    }
-                    public int getSalary () {
-                        public double getSalary () {
-                            return salary;
-                        }
-                        public void getOtdel ( int otdel){
-                            this.otdel = otdel;
-                            public int getid () {
-                                return id;
-                            }
-                            public void setOtdel ( int otdel){
-                                this.otdel = otdel;
-                            }
-                            public void setSalary ( int salary){
-                                public void setSalary ( double salary){
-                                    this.salary = salary;
-                                }
-
-                                public int getId () {
-                                    return id;
-                                }
-                                @Override
-                                public String toString () {
-                                    return "ФИО: " + fullName + " Отдел:" + otdel + " Зарплата: " + salary + " id: " + id;
-                                    return Worker{+"name='" + name + '\'' +", surname='" + surname + '\'' + ", midname='" + midname + '\'' + ", otdel=" + otdel +", salary=" + salary + ", id=" + id +};
-                                }
-                                public String printFullName () {
-                                    return Worker{+"name" + name + '\'' + ", surname='" + surname + '\'' + ", midname='" + midname + '\'' +};
-                                }
-                            }
-
-                        }
-                    }
-                }
-            }
-        }
+    public Worker(String fullName, int otdel, int salary) {
+        this.fullName = fullName;
+        this.otdel = otdel;
+        this.salary = salary;
+        this.id = idCount++;
     }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getOtdel() {
+        return otdel;
+    }
+
+    public void setOtdel(int otdel) {
+        this.otdel = otdel;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО: " + fullName + " Отдел:" + otdel + " Зарплата: " + salary + " id: " + id;
+    }
+
+    public String printFullName() {
+        return fullName;
+
+    }
+}
 
 
